6df1d9c1b1dfc3dffcf42c273e5e6ce66bd79cf3
gmanka <gmankab@gmail.com>
2025-08-05 10:04:08 +0500

    [-] cleanup

--- alsa-ucm-conf-xiaomi-nabu/prebuild.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION=$(grep Version: alsa-ucm-conf-xiaomi-nabu.spec | awk '{print $2}')
-
-rm -f "alsa-ucm-conf-xiaomi-nabu-$VERSION.tar.gz"
-tar -czf "alsa-ucm-conf-xiaomi-nabu-$VERSION.tar.gz" src
